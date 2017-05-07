77e5dd6dcdb9e6f681f7bae12e3af0debcc37281
EC2 Default User <ec2-user@ip-10-0-3-187.us-west-2.compute.internal>
2017-05-07 22:32:24 +0000

    added tenant2 server

---
+++ UMLParserTenant2/routes/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	
+	newLocation : './public/submissions/',
+	python : {
+		Command : 'java -jar /home/ec2-user/UMLParserTenant2/Java-Parser/parser.jar ',
+		inputFile : '/home/ec2-user/UMLParserTenant2/public/submissions',
+		outputFile : 'output.png',
+		outputLocation : '/home/ec2-user/UMLParserTenant2/public/submissions/out/output.png'
+	}
+}
